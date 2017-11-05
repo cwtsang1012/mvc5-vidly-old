15e4b5477448e68c08b89e8e8c7c7d439e2f27a8
Grace Tsang <33336177+cwtsang1012@users.noreply.github.com>
2017-11-06 00:49:30 +0800

    Complete Session 2 without doing excerise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -26,11 +27,15 @@ namespace Vidly.Controllers
         {
             //return Action resolved
             var movie = new Movie() { Name = "Shrek!" };
-            //return View(model: movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            var customers = new List<Customer>()
+            {
+                new Customer() { Name = "Customer 1" },
+                new Customer() { Name = "Customer 2" } 
+            };
+
+            var viewModel = new RandomMovieViewModel() { Movie = movie, Customers = customers };
+        
+            return View(viewModel);
         }
 
         public ActionResult Edit(int id)
